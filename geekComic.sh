ce77530d6ba4c1c03e44e3140744de01db257b29
Anubhav <anubhav@anubhav-Aspire-5738.(none)>
2013-11-07 12:50:06 +0530

    Improved file naming and added random fetching of PhDComic strips

--- geekComic.sh
+++ geekComic.sh
@@ -18,26 +18,31 @@ then
 	# Fetch xkcd strip
 	wget -q -O $dir/pageSource 'http://dynamic.xkcd.com/random/comic/';
 	imageLink=$(grep -o '\ http://imgs.xkcd.com/comics/[a-zA-Z0-9_.]*.png' $dir/pageSource);
-	imageName=`echo $imageLink | grep -o "[a-zA-Z0-9_]*.png"`;
+	imageName="XKCD_"`echo $imageLink | grep -o "[a-zA-Z0-9_]*.png"`;
 elif [ $n -eq 1 ]
 then
 	# fetch Cyanide & Happiness strip
 	wget -q -O $dir/pageSource 'http://www.explosm.net/comics/random/';
 	imageLink=$(grep -o 'http://www.explosm.net/db/files/Comics/[A-Za-z0-9/]*.png' $dir/pageSource);
-        imageName=`echo $imageLink | grep -o "[a-zA-Z0-9_]*.png"`;
+        imageName="Cyanide&Happiness_"`echo $imageLink | grep -o "[a-zA-Z0-9_]*.png"`;
 elif [ $n -eq 2 ]
 then
 	# fetch PhD Comics strip
-        wget -q -O $dir/pageSource 'http://phdcomics.com/comics.php';
-        imageLink=$(grep -o 'src=http://www.phdcomics.com/comics/archive/[A-Za-z0-9_]*.gif' $dir/pageSource | grep -P -o 'http://www.phdcomics.com/comics/archive/[A-Za-z0-9_]*.gif');
-        imageName=`echo $imageLink | grep -o "[a-zA-Z0-9_]*.gif"`;
+        wget -q -O $dir/pageSource 'http://www.phdcomics.com/comics/archive_list.php';
+        max_num_comics=$(grep -o 'http://www.phdcomics.com/comics/archive.php?comicid=[0-9]*' $dir/pageSource | grep -o [0-9]* | tail -1);
+        comicid=`echo "$RANDOM % $max_num_comics" | bc`;
+
+        wget -q -O $dir/pageSource 'http://www.phdcomics.com/comics/archive.php?comicid='$comicid;
+        imageLink=$(grep -o 'http://www.phdcomics.com/comics/archive/phd[0-9]*s.gif' $dir/pageSource | tail -1 );
+        imageName='PhDComics_'`echo $imageLink | grep -o "[a-zA-Z0-9_]*.gif"`;
+
 elif [ $n -eq 3 ]
 then
         # fetch Garfield Comics strip
         wget -q -O $dir/pageSource 'http://garfield.com/comic/random';
         imageLink=$(grep -o '/uploads/strips/[0-9-]*.jpg' $dir/pageSource);
 	imageLink='http://garfield.com'$imageLink; 
-        imageName=`echo $imageLink | grep -o "[a-zA-Z0-9_]*.jpg"`;
+        imageName="Garfield_"`echo $imageLink | grep -o "[a-zA-Z0-9_]*.jpg"`;
 fi
 
 
